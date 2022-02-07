ebef5b3d772784176dab0be94708f4ce2f9f867d
Yeonho Park <yeonho@deepnatural.ai>
2022-02-07 19:32:04 +0900

    fix to broadcast recieved push message not for Channel Talk

--- android/src/main/java/ai/deepnatural/channel_talk/PushInterceptService.java
+++ android/src/main/java/ai/deepnatural/channel_talk/PushInterceptService.java
@@ -3,11 +3,15 @@ package ai.deepnatural.channel_talk;
 import com.google.firebase.messaging.*;
 import com.zoyi.channel.plugin.android.ChannelIO;
 
+import android.content.Intent;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 import java.util.Map;
 
 public class PushInterceptService extends FirebaseMessagingService {
     private static final String TAG = "PushInterceptService";
+    public static final String ACTION_REMOTE_MESSAGE = "io.flutter.plugins.firebasemessaging.NOTIFICATION";
+    public static final String EXTRA_REMOTE_MESSAGE = "notification";
 
     @Override
     public void onNewToken(String refreshedToken) {
@@ -25,6 +29,9 @@ public class PushInterceptService extends FirebaseMessagingService {
             Log.d(TAG, "Channel Talk message received");
             ChannelIO.receivePushNotification(getApplication(), message);
         } else {
+            Intent intent = new Intent(ACTION_REMOTE_MESSAGE);
+            intent.putExtra(EXTRA_REMOTE_MESSAGE, remoteMessage);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
             Log.d(TAG, "Push message received, not for Channel Talk");
         }
     }
