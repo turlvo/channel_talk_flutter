638dd59f7a28e437c826b6ec431448615f5663f3
김찬울 <studio@gar.am>
2026-06-29 16:17:07 +0900

    Add iOS Swift Package Manager support

---
+++ ios/channel_talk_flutter/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "channel_talk_flutter",
+    platforms: [
+        .iOS("15.0"),
+    ],
+    products: [
+        .library(name: "channel-talk-flutter", targets: ["channel_talk_flutter"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/channel-io/channel-talk-ios-framework", exact: "13.0.2"),
+    ],
+    targets: [
+        .target(
+            name: "channel_talk_flutter",
+            dependencies: [
+                .product(name: "ChannelIOSDK", package: "channel-talk-ios-framework"),
+            ],
+            path: "Sources/channel_talk_flutter",
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ]
+        ),
+    ]
+)
